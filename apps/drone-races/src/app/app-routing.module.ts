925b79586271df2ede69c7452d010a40f6ae2e8a
Rensvdk20 <rensvdk@gmail.com>
2022-12-01 11:07:39 +0100

    Fixed file structure

--- apps/drone-races/src/app/app-routing.module.ts
+++ apps/drone-races/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { AboutComponent } from 'apps/drone-races/src/app/pages/about/about.component';
 
-import { MatchManagerDetailsComponent } from '@drone-races/features-ui/src/lib/match/match-manager-details/match-manager-details.component';
-import { MatchManagerEditComponent } from '@drone-races/features-ui/src/lib/match/match-manager-edit/match-manager-edit.component';
-import { MatchManagerOverviewComponent } from '@drone-races/features-ui/src/lib/match/match-manager-overview.component';
+import { MatchManagerDetailsComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-details/match-manager-details.component';
+import { MatchManagerEditComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-edit/match-manager-edit.component';
+import { MatchManagerOverviewComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-overview.component';
 
-import { UserManagerOverviewComponent } from '@drone-races/features-ui/src/lib/user/user-manager-overview.component';
-import { UserManagerDetailsComponent } from '@drone-races/features-ui/src/lib/user/user-manager-details/user-manager-details.component';
-import { UserManagerEditComponent } from '@drone-races/features-ui/src/lib/user/user-manager-edit/user-manager-edit.component';
+import { UserManagerOverviewComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-overview.component';
+import { UserManagerDetailsComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-details/user-manager-details.component';
+import { UserManagerEditComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-edit/user-manager-edit.component';
 
 const routes: Routes = [
 	{ path: '', component: HomepageComponent, pathMatch: 'full' },
--- apps/drone-races/src/app/app.module.ts
+++ apps/drone-races/src/app/app.module.ts
@@ -12,15 +12,15 @@ import { FooterComponent } from '@drone-races/shared/src/lib/components/footer/f
 
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
-import { UserManagerOverviewComponent } from '@drone-races/features-ui/src/lib/user/user-manager-overview.component';
-import { UserManagerListComponent } from '@drone-races/features-ui/src/lib/user/user-manager-list/user-manager-list.component';
-import { UserManagerDetailsComponent } from '@drone-races/features-ui/src/lib/user/user-manager-details/user-manager-details.component';
-import { UserManagerEditComponent } from '@drone-races/features-ui/src/lib/user/user-manager-edit/user-manager-edit.component';
+import { UserManagerOverviewComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-overview.component';
+import { UserManagerListComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-list/user-manager-list.component';
+import { UserManagerDetailsComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-details/user-manager-details.component';
+import { UserManagerEditComponent } from '@drone-races/features-ui/src/lib/user/user-manager/user-manager-edit/user-manager-edit.component';
 
-import { MatchManagerOverviewComponent } from '@drone-races/features-ui/src/lib/match/match-manager-overview.component';
-import { MatchManagerListComponent } from '@drone-races/features-ui/src/lib/match/match-manager-list/match-manager-list.component';
-import { MatchManagerDetailsComponent } from '@drone-races/features-ui/src/lib/match/match-manager-details/match-manager-details.component';
-import { MatchManagerEditComponent } from '@drone-races/features-ui/src/lib/match/match-manager-edit/match-manager-edit.component';
+import { MatchManagerOverviewComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-overview.component';
+import { MatchManagerListComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-list/match-manager-list.component';
+import { MatchManagerDetailsComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-details/match-manager-details.component';
+import { MatchManagerEditComponent } from '@drone-races/features-ui/src/lib/match/match-manager/match-manager-edit/match-manager-edit.component';
 import { AboutComponent } from './pages/about/about.component';
 @NgModule({
 	declarations: [
