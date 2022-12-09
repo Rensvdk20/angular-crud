9bbefa12562abf3335a26e21bd5c4d2bec62e3e0
Rensvdk20 <rensvdk@gmail.com>
2022-12-09 15:25:55 +0100

    Fixed RacerDocument naming

--- apps/data-api/src/app/racer/racer.schema.ts
+++ apps/data-api/src/app/racer/racer.schema.ts
@@ -1,9 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
 import { Drone } from '../drone/drone.schema';
 
-export type UserDocument = Racer & Document;
+export type RacerDocument = Racer & Document;
 
 @Schema()
 export class Racer {
