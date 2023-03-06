114b6f9869bb5071a7ef5ce57cd52abc7ee6207e
Rensvdk20 <rensvdk@gmail.com>
2023-03-06 17:22:00 +0100

    Added cypress for data-api testing

--- apps/data-api-e2e/src/e2e/app.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('data-api', () => {
-	it('should get all matches', () => {
-		cy.request(`localhost:3333/data-api/match`).then((response) => {
-			expect(response.status).to.eq(200);
-			expect(response.body).to.be.at.least(1);
-		});
-	});
-});
